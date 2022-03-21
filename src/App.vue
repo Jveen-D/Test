bb3fe2928479809a3f0321f8a7bee772eeabfc7a
杜文杰 <d85066965@icloud.com>
2022-03-21 22:31:13 +0800

    Init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+
+<!--
+ * @Date: 2022-03-21 13:58:42
+ * @LastEditors: dwj18066042960
+ * @FilePath: \Test\src\App.vue
+-->
+<template>
+  <div class="h-[100px] bg-green-200"></div>
+  <router-view class="mt-[200px]"></router-view>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
---
+++ src/components/A.vue
@@ -0,0 +1,32 @@
+<!--
+ * @Date: 2022-03-21 14:21:02
+ * @LastEditors: dwj18066042960
+ * @FilePath: \Test\src\components\A.vue
+-->
+<template>
+    <div
+        ref="el"
+        :style="style"
+        style="touch-action:none;"
+        class="fixed flex flex-col items-center justify-center py-2 w-[200px] bg-white select-none cursor-move"
+    >
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <div class="text-black">👋 Drag me!</div>
+        <div
+            class="text-sm text-gray-800 opacity-50"
+        >I am at {{ Math.round(x) }}, {{ Math.round(y) }}</div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { useDraggable } from '@vueuse/core';
+import { isClient } from '@vueuse/shared';
+import { ref } from 'vue';
+
+const innerWidth = isClient ? window.innerWidth : 200
+const el = ref(null)
+const { x, y, style } = useDraggable(el, {
+    initialValue: { x: innerWidth / 4.2, y: 0 },
+    preventDefault: true,
+})
+</script>
