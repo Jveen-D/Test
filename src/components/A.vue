42152c45fb7d70d426e92f3a64fdcae46c84c52e
杜文杰 <d85066965@icloud.com>
2022-03-21 22:41:32 +0800

    finish

--- src/components/A.vue
+++ src/components/A.vue
@@ -1,20 +1,23 @@
+
 <!--
  * @Date: 2022-03-21 14:21:02
  * @LastEditors: dwj18066042960
  * @FilePath: \Test\src\components\A.vue
 -->
 <template>
-    <div
-        ref="el"
-        :style="style"
-        style="touch-action:none;"
-        class="fixed flex flex-col items-center justify-center py-2 w-[200px] bg-white select-none cursor-move"
-    >
-        <img alt="Vue logo" src="../assets/logo.png" />
-        <div class="text-black">👋 Drag me!</div>
+    <div>
         <div
-            class="text-sm text-gray-800 opacity-50"
-        >I am at {{ Math.round(x) }}, {{ Math.round(y) }}</div>
+            ref="el"
+            :style="style"
+            style="touch-action:none;"
+            class="fixed flex flex-col items-center justify-center py-2 w-[200px] bg-white select-none cursor-move"
+        >
+            <img alt="Vue logo" src="../assets/logo.png" />
+            <div class="text-black">👋 Drag me!</div>
+            <div
+                class="text-sm text-gray-800 opacity-50"
+            >I am at {{ Math.round(x) }}, {{ Math.round(y) }}</div>
+        </div>
     </div>
 </template>
 
